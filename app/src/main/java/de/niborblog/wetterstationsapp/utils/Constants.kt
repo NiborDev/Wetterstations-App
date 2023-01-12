dc8a9e09a8e3f111236c8c3fed9d09334a5678c9
NiborDev <38883662+NiborDev@users.noreply.github.com>
2023-01-12 17:21:27 +0100

    Update Constants.kt

--- app/src/main/java/de/niborblog/wetterstationsapp/utils/Constants.kt
+++ app/src/main/java/de/niborblog/wetterstationsapp/utils/Constants.kt
@@ -8,7 +8,7 @@ package de.niborblog.wetterstationsapp.utils
 
 object Constants {
     const val BASE_URL = "https://api.weatherapi.com/"
-    const val API_KEY = "00c162b831094bb9b65172119222304"
+    const val API_KEY = ""
     const val LANG = "de"
     const val HARDWARE_NAME = "WetterStation by M&R"
     const val SCAN_PERIOD = 1000L
